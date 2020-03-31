7e1d7877bb6ec49883b61793dfe123af9af52d11
JeromeDecore <62428742+JeromeDecore@users.noreply.github.com>
2020-03-31 17:28:13 +0200

    Amélioration accès tableau

--- PROJ_2_1/Src/TableTrigo.s
+++ PROJ_2_1/Src/TableTrigo.s
@@ -8,13 +8,11 @@
 	
 
 somme proc
-	mov r1, #2
-	mul r0, r0, r1
-;i est dans r0, @TabCos dans r1, @TabSin dans r2
+	;i est dans r0, @TabCos dans r1, @TabSin dans r2
 	ldr  r1, =TabCos
 	ldr  r2, =TabSin
-	ldrsh r1, [r1,r0]
-	ldrsh r2, [r2,r0]
+	ldrsh r1, [r1,r0, LSL #1]
+	ldrsh r2, [r2,r0, LSL #1]
 	mul r3, r1, r1
 	mla r0, r2, r2, r3
 	bx  lr
