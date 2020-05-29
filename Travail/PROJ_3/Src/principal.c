f90ab66333f00fe1958b93f7ed44fc33410c0fe1
JeromeDecore <62428742+JeromeDecore@users.noreply.github.com>
2020-05-29 16:09:04 +0200

    Objectif 2

--- Travail/PROJ_3/Src/principal.c
+++ Travail/PROJ_3/Src/principal.c
@@ -1,5 +1,5 @@
-#include <gassp72.h>
-#define SYSTICK_PER 360000
+#include "gassp72.h"
+#define SYSTICK_PER 14400
 #define M2TIR 985661
 #define N 64
 
@@ -10,51 +10,63 @@ void sys_callback();
 
 unsigned short dma_buf[N];
 int compteur_occurences[6];
+int occurences_min = 3;
 int tab_k[6] = {17, 18, 19, 20, 23, 24};
+int resu;
 
 
 void sys_callback(){
-	int resu;
 	// Démarrage DMA pour 64 points
 	Start_DMA1(64);
 	Wait_On_End_Of_DMA1();
 	Stop_DMA1;
 	for(int i=0; i<6; i++){
 		resu = dft((int *)dma_buf, tab_k[i]);
-		if(resu > M2TIR)
-			compteur_occurences[i] += 1;
+		if( resu >= M2TIR)
+			compteur_occurences[i] ++;
 		else	
 			compteur_occurences[i] = 0;
 	}
-	GPIO_Set(GPIOB, 1);
-	GPIO_Clear(GPIOB, 1);
+	int laser = 0;
+	int i = 0;
+	while( i < 6 && !laser){
+		laser = compteur_occurences[i] >= occurences_min;
+		i++;
+	}
+	if(laser)
+		GPIO_Set(GPIOB, 14);
+	else
+		GPIO_Clear(GPIOB, 14);
 }
 
 
 int main(void)
 {
-	// activation de la PLL qui multiplie la fréquence du quartz par 9
-	CLOCK_Configure();
-	// PA2 (ADC voie 2) = entrée analog
-	GPIO_Configure(GPIOA, 2, INPUT, ANALOG);
-	// PB1 = sortie pour profilage à l'oscillo
-	GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
-	// PB14 = sortie pour LED
-	GPIO_Configure(GPIOB, 14, OUTPUT, OUTPUT_PPULL);
+	GPIO_Clear(GPIOB, 14);
+	while(1){
+		// activation de la PLL qui multiplie la fréquence du quartz par 9
+		CLOCK_Configure();
+		// PA2 (ADC voie 2) = entrée analog
+		GPIO_Configure(GPIOA, 2, INPUT, ANALOG);
+		// PB1 = sortie pour profilage à l'oscillo
+		GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
+		// PB14 = sortie pour LED
+		GPIO_Configure(GPIOB, 14, OUTPUT, OUTPUT_PPULL);
 
-	// activation ADC, sampling time 1us
-	Init_TimingADC_ActiveADC_ff( ADC1, 0x33 );
-	Single_Channel_ADC( ADC1, 2 );
-	// Déclenchement ADC par timer2, periode (72MHz/320kHz)ticks
-	Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
-	// Config DMA pour utilisation du buffer dma_buf (a créér)
-	Init_ADC1_DMA1( 0, dma_buf );
+		// activation ADC, sampling time 1us
+		Init_TimingADC_ActiveADC_ff( ADC1, 82);
+		Single_Channel_ADC( ADC1, 2 );
+		// Déclenchement ADC par timer2, periode (72MHz/320kHz)ticks
+		Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
+		// Config DMA pour utilisation du buffer dma_buf (a créér)
+		Init_ADC1_DMA1( 0, dma_buf );
 
-	// Config Timer, période exprimée en périodes horloge CPU (72 MHz)
-	Systick_Period_ff( SYSTICK_PER );
-	// enregistrement de la fonction de traitement de l'interruption timer
-	// ici le 3 est la priorité, sys_callback est l'adresse de cette fonction, a créér en C
-	Systick_Prio_IT( 3, sys_callback );
-	SysTick_On;
-	SysTick_Enable_IT;
+		// Config Timer, période exprimée en périodes horloge CPU (72 MHz)
+		Systick_Period_ff( SYSTICK_PER );
+		// enregistrement de la fonction de traitement de l'interruption timer
+		// ici le 3 est la priorité, sys_callback est l'adresse de cette fonction, a créér en C
+		Systick_Prio_IT( 3, sys_callback );
+		SysTick_On;
+		SysTick_Enable_IT;
+	}
 }
